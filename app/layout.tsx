991002e8887ea49795394a69027a034c42465c8b
Oleksandr Darchuk <oleksandr.darchuk@altexsoft.com>
2024-08-05 23:58:05 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
-export default function RootLayout({
-  children,
-}: {
+import { FC } from "react";
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
+interface IRootLayout {
   children: React.ReactNode;
-}) {
+}
+
+const RootLayout: FC<IRootLayout> = ({ children }) => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
---
+++ app/ui/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Page: FC = () => {
+  return <div>Customers Pge</div>;
+};
+
+export default Page;
